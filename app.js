d011286cfa1909af82b1d215429cdb270a56a16e
Aashrith Sriramadas <aashrithsriramadas@gmail.com>
2023-06-23 16:23:29 +0530

    added mongodb

--- app.js
+++ app.js
@@ -1,69 +1,75 @@
+//jshint esversion:6
+
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-const  _ = require("lodash");
-
+const _ = require("lodash");
+const mongoose =require("mongoose");
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 const app = express();
 
-let posts =[];
-
 app.set('view engine', 'ejs');
+
+mongoose.connect("mongodb+srv://Aashrith:Aashrith@cluster0.nqgy9z7.mongodb.net/blogsite");
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 
-app.get("/",function(req,res){
-  res.render("home",{
-    title:"HOME",
-    homecontent:homeStartingContent,
-    lists : posts
+const blogSchema = {
+  title :String ,
+  content : String 
+}
+const Blog = mongoose.model("Blog",blogSchema);
+
+
+app.get("/", async function(req, res){
+  const temp =  await Blog.find({})
   
- });
+  res.render("home", {
+    startingContent: homeStartingContent,
+    posts: temp
+    });
 });
 
+app.get("/about", function(req, res){
+  res.render("about", {aboutContent: aboutContent});
+});
 
-app.get("/contact",function(req,res){
-  res.render("home",{title:"Contact",
-  homecontent:contactContent,
-  lists : []});
-})
-app.get("/about",function(req,res){
-  res.render("home",{title:"About",
-  homecontent:aboutContent,
-  lists : []});
-})
-app.get("/compose",function(req,res){
-  res.render("compose",{aboutParagraph : aboutContent});
-})
-app.post("/compose",function(req,res){
-  
-  const post = {
-    title : req.body.titleText,
-    content : req.body.postText
-  };
-  posts.push(post);
+app.get("/contact", function(req, res){
+  res.render("contact", {contactContent: contactContent});
+});
+
+app.get("/compose", function(req, res){
+  res.render("compose");
+});
+
+app.post("/compose", function(req, res){
+  const post = new Blog ({
+    title: req.body.postTitle,
+    content: req.body.postBody
+  });
+  //console.log(post);
+  post.save();
+
   res.redirect("/");
-})
-
-app.get("/posts/:title",function(req,res){
-
-  const title = req.params.title;
-
-  posts.forEach( element => { console.log(element);
-    if(_.lowerCase(element.title)===_.lowerCase(title)) res.render("home",{lists:[],homecontent:element.content,title:element.title})
-    
-    ;})
-    
-
 
 });
 
+app.get("/posts/:postId",async function(req, res){
+  const requestedId = (req.params.postId);
+ const temp = await Blog.findById( requestedId);
 
+ 
+  res.render("post", {
+          title: temp.title,
+           content: temp.content
+        });
+ })
+ 
 
 
 
